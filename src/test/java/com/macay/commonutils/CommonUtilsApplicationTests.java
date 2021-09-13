40cec2a148c1ab94c0f4f6c11fc9edc5c38c8e23
macay <1207260521@qq.com>
2021-09-14 00:34:56 +0800

    init add

---
+++ src/test/java/com/macay/commonutils/CommonUtilsApplicationTests.java
@@ -0,0 +1,13 @@
+package com.macay.commonutils;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class CommonUtilsApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
